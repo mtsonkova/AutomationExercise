8b116ed0ba26108bbaea1d1b9912a85aca7fddd7
mtsonkova <mtsonkova950@gmail.com>
2025-09-11 22:07:05 +0300

    AutomationExercise
    added src folder with lovators, pages, utils subfolders
    created texts.json file to hold different types of messages used inside the web app - errors, confirmation messages, etc..
    created users.json file to hold personal information for default user - purpose to be imported from file in test cases, not hardcoded.

---
+++ src/locators/loginSignUpLocators.js
@@ -0,0 +1,14 @@
+export const loginSignUpLocators = (page) => ({
+loginForm: {
+    email: page.locator('[data-qa="login-email"]'),
+    password: page.locator('[data-qa="login-password"]'),
+    loginBtn: page.locator('[data-qa="login-button"]'),
+},
+
+registerForm: {
+    name: page.locator('[data-qa="signup-name"]'),
+    email: page.locator('[data-qa="signup-email"]'),
+    signUpBtn: page.locator('[data-qa="signup-button"]'),
+}
+
+})
---
+++ tests/login.spec.js
@@ -0,0 +1,5 @@
+// Login with valid credentials
+// login with incorrect email -> missing @ and check the error message
+// login with empty email
+// login with empty password
+// login with wrong credentials
