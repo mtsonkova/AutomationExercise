692969ef4e5aded5d19f536d51387440de008fdb
mtsonkova <mtsonkova950@gmail.com>
2025-09-12 19:01:32 +0300

    Added cart locators

---
+++ src/locators/cartLocators.js
@@ -0,0 +1,11 @@
+export const CartLocators = (page) => ({
+proceedToCheckoutBtn: page.getByRole('link', {name: 'Proceed To Checkout'}),
+productsTable: page.locator('#cart_info_table'),
+itemImage: page.locator('tbody td.cart_product a'),
+productName: page.locator('tbody td.cart_description h4 a'),
+productCategory: page.locator('tbody td.cart_description p'),
+productPrice: page.locator('tbody td.cart_price p'),
+productQty: page.locator('tbody td.cart_quantity button'),
+productTotal:page.locator('tbody td.cart_total p'),
+deleteProduct: page.locator('tbody td.cart_delete a'),
+});
